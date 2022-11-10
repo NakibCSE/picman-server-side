1aeb8ec1371b3cc2666d0eb79c87a28a290f5c19
Nakib <nakkibcse@gmail.com>
2022-11-10 20:47:28 +0600

    mongo connected

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express=require('express')
 const cors=require('cors')
 const app=express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.PORT || 5000
 
 app.use(cors())
+app.use(express.json())
+
+//user: assignment-11
+//password: JZQ95cfiCQAwXfTn
+
+
+app.get('/',(req,res)=>{
+    res.send('server is running')
+})
+
+
+const uri = "mongodb+srv://assignment-11:<password>@cluster0.8gqynbm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+
+    }
+    finally{
+        run().catch(console.dir)
+    }
+}
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
