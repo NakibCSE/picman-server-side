126f578f936ca08e667ee2a96dc347ddd070f617
Nakib <nakkibcse@gmail.com>
2022-11-10 23:01:55 +0600

    add method

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const cors=require('cors')
 const app=express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port=process.env.PORT || 5000
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
@@ -16,16 +17,7 @@ app.get('/',(req,res)=>{
 })
 
 
-const uri = "mongodb+srv://assignment-11:<password>@cluster0.8gqynbm.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
 
-    }
-    finally{
-        run().catch(console.dir)
-    }
-}
 
 
 
